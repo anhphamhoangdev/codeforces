215998d05973008db9faa5280aa7e913d78a4f4c
Anh Pham Hoang <135268449+anhphamhoangdev@users.noreply.github.com>
2023-12-08 22:14:34 +0700

    Create A. Amusing Joke.cpp

---
+++ Problem A/A. Amusing Joke.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+ 
+#define faster() ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define tcase() int t; cin>>t
+#define tloop() while(t--)
+#define FOR(i,a,b) for(int i = a ; i < b ; i ++)
+#define FORsub(i,a,b) for(int i = a ; i >= b ; i--)
+ 
+int main()
+{
+    faster();
+    string s1; cin >> s1;
+    string s2; cin >> s2;
+    int size = s1.length() + s2.length();
+    s1.append(s2);
+    string s3; cin >> s3;
+    sort(s1.begin(),s1.end());
+    sort(s3.begin(),s3.end());
+    if(s3.length() > size)
+    {
+        cout << "NO";
+        return 0;
+    }
+    FOR(i,0,size)
+    {
+        if(s1[i] != s3[i])
+        {
+            cout << "NO";
+            return 0;
+        }
+    }
+    cout << "YES";
+}
